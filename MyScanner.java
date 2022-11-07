6817bf5862ef33421e77d27595ce0cfa4ea6a6f7
Yusef McCaskill <yusef.mccaskill@mrjaffesclass.com>
2022-11-07 14:25:53 -0800

    initial commit

---
+++ MyScanner.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MyScanner
+{
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter(",|/n");
+        while (true) {
+            String nxt = scanner.next();
+            System.out.println(nxt);
+        }
+    }
+
+    public static void main(String[] args) {
+        MyScanner myScanner = new MyScanner();
+        myScanner.run();
+    }
+}
